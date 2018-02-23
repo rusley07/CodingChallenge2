f2cce441e8311bcda7ce9e601af10c6aff94a058
Ruslan Batura <ruslanbatura07081980@hotmail.com>
2018-02-23 15:43:32 +1100

    Add project files.

---
+++ CodingChallenge2/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingChallenge2.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        Letter[] letters = new Letter[]
+        {
+            new Letter { Id = 0, Char = "A" },
+            new Letter { Id = 1, Char = "B" },
+            new Letter { Id = 2, Char = "C" },
+            new Letter { Id = 3, Char = "D" }
+        };
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Letter> Get()
+        {
+            return letters;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
